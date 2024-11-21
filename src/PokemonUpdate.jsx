6468abd40a68a94ceeccd7d3fae13cebabe7781e
Burak Durmuşoğlu <burak2482@gmail.com>
2024-11-21 19:18:27 +0300

    update page ui improvements

--- src/PokemonUpdate.jsx
+++ src/PokemonUpdate.jsx
@@ -11,34 +11,34 @@ export default function PokemonUpdate() {
   };
 
   return (
-    <div>
-      <div>
+    <div className="flex flex-col justify-center items-center min-h-svh w-full h-full bg-slate-100">
+      <div className="flex flex-col w-1/4 h-1/4 py-10 px-60 justify-center items-center bg-slate-300 rounded-3xl">
         <form onSubmit={handleSubmit}>
-          <h2>Add Pokemon</h2>
-          <div>
-            <label>Name</label>
+          <h2 className="font-semibold text-3xl text-center">Add Pokemon</h2>
+          <div className="flex flex-col mt-5">
+            <label className="font-semibold text-xl">Name</label>
             <input
               type="text"
               placeholder="Enter name"
-              className="form-control"
+              className="border border-x-2 px-10 text-left mt-2 font-semibold"
               value={name}
               onChange={(e) => setName(e.target.value)} // Kullanıcı adı girerken state güncelleniyor
             />
           </div>
-          <div>
-            <label>Alias</label>
+          <div  className="flex flex-col mt-5">
+            <label className="font-semibold text-xl">Alias</label>
             <input
               type="text"
               placeholder="Enter alias"
-              className="form-control"
+              className="border border-x-2 px-10 text-left mt-2 font-semibold"
               value={alias}
               onChange={(e) => setAlias(e.target.value)} // Kullanıcı alias girerken state güncelleniyor
             />
           </div>
-          <div>
-            <label>Elements</label>
+          <div className="flex flex-col mt-5">
+            <label  className="font-semibold text-xl mb-2">Elements</label>
             <select
-              value={element}
+              value={element} className="border border-x-2 px-10 text-left mt-2 font-semibold"
               onChange={(e) => setElement(e.target.value)} // Kullanıcı element seçerken state güncelleniyor
             >
               <option value="">Select Element</option>
@@ -49,7 +49,9 @@ export default function PokemonUpdate() {
               <option value="Dark">Dark</option>
             </select>
           </div>
-          <button type="submit">Submit</button>
+          <div className="flex flex-col items-center">
+          <button type="submit" className="px-10 py-2 rounded-full font-semibold bg-slate-500 mt-7 text-white ">Submit</button>
+          </div>
         </form>
       </div>
     </div>
