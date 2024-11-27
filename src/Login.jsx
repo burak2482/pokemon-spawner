c995d6111c9390c634e16a4f7f06b7a4c5b21beb
Burak Durmuşoğlu <burak2482@gmail.com>
2024-11-27 13:53:14 +0300

    login and signup pages ui improving

--- src/Login.jsx
+++ src/Login.jsx
@@ -11,16 +11,18 @@ const Signup = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <h3>Log in</h3>
+    <div className="flex flex-col justify-center items-center bg-slate-400 min-h-screen">
+      <div className="flex flex-col justify-center items-center w-1/4 h-full py-24 px-48 bg-slate-500 rounded-3xl">
+        <form onSubmit={handleSubmit} className="flex flex-col justify-center gap-2 bg-slate-500 text-black">
+          <h3 className="font-semibold text-2xl text-center mb-3 text-white">Log in</h3>
 
-        <label>Email:</label>
-        <input type='email' value={email} onChange={(e) => setEmail(e.target.value)}></input>
-        <label>Password:</label>
-        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)}></input>
-        <button>Sign up</button>
-      </form>
+          <label className="font-semibold text-xl mb-3 text-white">Email:</label>
+          <input type='email' placeholder="E-mail"className="border border-x-2 px-6 text-left font-semibold" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+          <label className="font-semibold text-xl mb-3 text-white">Password:</label>
+          <input type='password' placeholder="Password"className="border border-x-2 px-6 text-left font-semibold" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+          <button className="py-2 px-10 bg-slate-700 font-semibold text-white rounded-full mt-5 active:bg-slate-600">Log in</button>
+        </form>
+      </div>
     </div>
   )
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <header>
-      <div className="flex flex-row justify-between items-center py-4 px-16">
-        <Link to="/" className="font-semibold text-3xl">
+      <div className="flex flex-row justify-between items-center py-2 md:py-4 md:px-16">
+        <Link to="/" className="font-semibold text-xl ml-3 md:text-3xl text-nowrap">
           Pokemon Spawner
         </Link>
         <nav>
           <div>
-            <Link to="/login" className="mr-16 text-2xl font-semibold">Login</Link>
-            <Link to="/signup" className="mr-10 text-2xl font-semibold">Signup</Link>
+            <Link to="/login" className="mr-2 text-lg md:mr-16 md:text-2xl font-semibold px-2 border-2 border-lime-400 rounded-sm">Login</Link>
+            <Link to="/signup" className="mr-2 text-lg md:mr-10 md:text-2xl font-semibold px-2 border-2 border-lime-400 rounded-sm">Signup</Link>
           </div>
         </nav>
       </div>
